6d2186b00187c85dab8c205d5910d6d26291b283
ClaudioChametla <clauchametla.777@gmail.com>
2021-10-05 18:40:17 -0500

    7.- Validaciones del GPS

--- app/src/main/java/com/chametla/localizacioncombitizayuca/UserMap.java
+++ app/src/main/java/com/chametla/localizacioncombitizayuca/UserMap.java
@@ -1,15 +1,37 @@
 package com.chametla.localizacioncombitizayuca;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationRequest;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -23,6 +45,30 @@ public class UserMap extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap mMap;
     SupportMapFragment mMapFragment;
 
+    com.google.android.gms.location.LocationRequest mLocationRequest;
+    FusedLocationProviderClient mFusedLocation;
+    private final static int LOCATION_REQUEST_CODE = 1;
+    private final static int SETTINGS_REQUEST_CODE = 2;
+
+    //Callback lines
+    LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            for (Location location : locationResult.getLocations()) {
+                if (getApplicationContext() != null) {
+                    //Obtener locación en tiempo real
+                    mMap.moveCamera(CameraUpdateFactory.newCameraPosition(
+                            new CameraPosition.Builder()
+                                    .target(new LatLng(location.getLatitude(), location.getLongitude()))
+                                    .zoom(15f)
+                                    .build()
+                    ));
+                }
+            }
+        }
+    };
+    //Callback lines ends
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,28 +77,152 @@ public class UserMap extends AppCompatActivity implements OnMapReadyCallback {
         mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mMapFragment.getMapAsync(this::onMapReady);
 
+
+        //Instanciar propiedades de Firebase
         mAtuh = FirebaseAuth.getInstance();
         mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-
+        //Instancias boton cerrar
         botonCerrar = findViewById(R.id.bCerrar);
+        //Instancia Fused Location
+        mFusedLocation = LocationServices.getFusedLocationProviderClient(this);
 
+
+        //Metodo de boton
         botonCerrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 cerrarSesion();
             } //Boton cerrar sesion
-            private void cerrarSesion(){
+
+            private void cerrarSesion() {
                 mAtuh.signOut(); // Cierra sesión en Firebase
-                Intent intent = new Intent(UserMap.this,SelectOptionLogin.class);
+                Intent intent = new Intent(UserMap.this, SelectOptionLogin.class);
                 startActivity(intent);
             }
         });
+        //Fin metodo boton
     }
 
+
+    //Metodo OnMap sobreescrito
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+
+        mLocationRequest = new com.google.android.gms.location.LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.QUALITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(5);
+
+        startlocation();
 
     }
+    //Fin metodo OnMap
+
+
+    //Permisos ubicacion
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        mFusedLocation.requestLocationUpdates(mLocationRequest,mLocationCallback, Looper.myLooper());
+                    }
+                }
+            else {
+                    checkLocationPermission();
+                }
+        } else {
+                checkLocationPermission();
+                }
+    }
+
+
+    /*Revisar GPS activo
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SETTINGS_REQUEST_CODE && gpsActivated()) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mFusedLocation.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+        }
+        else{
+            showNoGps();
+        }
+    }
+
+    private void showNoGps(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Activar ubicación para continuar")
+                .setPositiveButton("Configuarción", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        startActivityForResult(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), SETTINGS_REQUEST_CODE );
+                    }
+                })
+                .create()
+                .show();
+    }
+
+
+    private boolean gpsActivated(){
+        boolean isActive = false;
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            isActive = true;
+        }
+            return isActive;
+    }*/
+//FIn Permisos activos
+
+    private void startlocation(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+                mFusedLocation.requestLocationUpdates(mLocationRequest,mLocationCallback, Looper.myLooper());
+            }
+            else {
+                checkLocationPermission();
+            }
+        }
+        else{
+                mFusedLocation.requestLocationUpdates(mLocationRequest,mLocationCallback, Looper.myLooper());
+        }
+
+    }
+
+
+
+    private void  checkLocationPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+                new AlertDialog.Builder(this).setTitle("Proporciona los permisos para continuar.").setMessage("Se requiere los permisos de ubicación")
+                        .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(UserMap.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_REQUEST_CODE);
+                            }
+                        })
+                        .create()
+                        .show();
+
+            }
+            else{
+                ActivityCompat.requestPermissions(UserMap.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_REQUEST_CODE);
+            }
+
+        }
+    }
+    //Fin codigo para permisos
 }
